8650bbca1ca24e0ae5ae89603987077981a4d151
IJosueeh <royer.tanta27@gmail.com>
2024-03-15 16:04:04 -0500

    Update Nav & Fixed nav open and closed a click and html menú

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,24 @@
 const buttonNav = document.querySelector(".nav-toggler");
 const navMenu = document.querySelector(".nav-menu");
+const navLinks = navMenu.querySelectorAll("li a");
+
+function navClose(){
+  navMenu.classList.remove("show")
+}
+
+navLinks.forEach(link => {
+  link.addEventListener("click", navClose);
+});
 
 buttonNav.addEventListener("click", () => {
   navMenu.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".nav-container");
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
